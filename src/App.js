969f233f5ae9d0468f6b27ec794cd8a98c573126
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-11-29 16:21:25 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Details from './pages/Details';
 
-function App() {
+export default function App() {
   return (
-    <>
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/details" component={Details} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
-
-export default App;
