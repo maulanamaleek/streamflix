af2011051aa1c96755617837afcfda8712b22780
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-11-29 16:22:12 +0700

    add content and fix props

--- src/components/molecules/display/index.js
+++ src/components/molecules/display/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Poster, Title } from '../../atoms';
+import './displayMovies.scss';
 
-export default function Display({ src, title }) {
+export default function Display({
+  src, title, price, click,
+}) {
   return (
-    <div>
-      <Poster src={src} />
-      <Title title={title} />
+    <div className="display-movies">
+      <div className="movie-card">
+        <>
+          <Poster className="poster" src={src} />
+          <Title className="title" title={title} />
+          <p>
+            Rp
+            {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+          </p>
+          <Link to="/details">
+            <button type="button" className="button" onClick={click}>Details</button>
+          </Link>
+        </>
+      </div>
     </div>
   );
 }
@@ -14,4 +29,6 @@ export default function Display({ src, title }) {
 Display.propTypes = {
   src: PropTypes.node.isRequired,
   title: PropTypes.node.isRequired,
+  price: PropTypes.node.isRequired,
+  click: PropTypes.node.isRequired,
 };
