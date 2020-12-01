b23164f90ffd6e1c5dbc23a405648c46dd93510d
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 23:24:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Collection, Details, Main } from './pages';
 import MovieProvider from './utils/MovieContext';
 
@@ -7,12 +7,14 @@ export default function App() {
   return (
     <div>
       <MovieProvider>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route path="/page/:page" component={Main} />
-          <Route path="/collection" component={Collection} />
-          <Route path="/:movieID/:slug" component={Details} />
-        </Switch>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/page/:page" component={Main} />
+            <Route path="/collection" component={Collection} />
+            <Route path="/:movieID/:slug" component={Details} />
+          </Switch>
+        </Router>
       </MovieProvider>
     </div>
   );
