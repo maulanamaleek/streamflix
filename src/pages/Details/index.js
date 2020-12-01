c140ca217df13e84a54b091e027e050bbe71fa90
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 20:54:52 +0700

    add loading

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { Navbar } from '../../components/molecules';
-// import axios from 'axios';
+import Star from '../../assets/star.png';
+import Time from '../../assets/time.png';
 import { MovieContext } from '../../utils/MovieContext';
 import './details.scss';
 
 class Details extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     btn: 'Purchase',
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false }), 1000);
+  }
 
   switchButton = (id) => {
     const localOwned = JSON.parse(localStorage.getItem('Owned'));
     let btnText = '';
-    // const { btn } = this.state;
 
     if (!localOwned) {
       btnText = 'Purchase';
@@ -32,14 +36,31 @@ class Details extends Component {
         btnText = 'Purchase';
       }
     }
-    // window.location.reload();
-    // this.setState({ btn: btnText });
+
     return btnText;
+  };
+
+  displayCast = () => {
+    const Cast = JSON.parse(localStorage.getItem('Cast'));
+    const casts = Cast.splice(0, 4);
+    const castName = [];
+
+    for (let i = 0; i < 4; i += 1) {
+      castName.push(casts[i].name);
+    }
+
+    return castName.join(', ');
   };
 
   render() {
     const Movie = JSON.parse(localStorage.getItem('Movie'));
     const Price = JSON.parse(localStorage.getItem('Price'));
+    const { loading } = this.state;
+
+    if (loading) {
+      return <div className="loading"> </div>;
+    }
+
     return (
       <div className="details">
         <Navbar />
@@ -51,14 +72,24 @@ class Details extends Component {
                 <img src={`https://image.tmdb.org/t/p/w200/${Movie.poster_path}`} alt={Movie.title} />
               </div>
               <div className="details-right">
-                <h3>{Movie.title}</h3>
-                <p>{Movie.vote_average}</p>
+                <h3 className="details-title">{Movie.title}</h3>
+                <div className="vote">
+                  <img src={Star} alt="vote" />
+                  <span>{Movie.vote_average}</span>
+                </div>
+                <div className="time">
+                  <img src={Time} alt="time" />
+                  <span>{`${Movie.runtime} minutes`}</span>
+                </div>
+                <h3 className="details-overview">Overview</h3>
+                <p className="overview">{Movie.overview}</p>
                 <p>
-                  {`${Movie.runtime} minutes`}
+                  Casts:
+                  {' '}
+                  {this.displayCast()}
                 </p>
-                <p>{Movie.overview}</p>
                 <button type="button" onClick={() => context.purchase(Movie)}>{this.switchButton(Movie.id)}</button>
-                <span>{`Rp ${Price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`}</span>
+                <span className="movie-price">{`Rp ${Price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`}</span>
               </div>
             </div>
           )}
