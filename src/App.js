d1e5969c159523d9c547367d3f239dcae9bd24d1
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 23:43:36 +0700

    use hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Collection, Details, Main } from './pages';
 import MovieProvider from './utils/MovieContext';
 
