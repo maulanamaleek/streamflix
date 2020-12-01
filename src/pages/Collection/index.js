5a8dfe4737bbbca645639bb6e31dff468b4f8163
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 10:50:29 +0700

    create collection page

---
+++ src/pages/Collection/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from '../../components/molecules';
+
+export default function Collection() {
+  return (
+    <div>
+      <Navbar />
+      <h1>This is Collection!</h1>
+    </div>
+  );
+}
