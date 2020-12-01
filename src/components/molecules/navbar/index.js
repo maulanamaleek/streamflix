a5c5b8b81a3c9563a13c78c8472e45b1a6fa2c4f
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 10:50:02 +0700

    add coin on navbar

--- src/components/molecules/navbar/index.js
+++ src/components/molecules/navbar/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { MovieContext } from '../../../utils/MovieContext';
 import Logo from '../../../assets/logo.png';
+import Coins from '../../../assets/coins.png';
 import './navbar.scss';
 
 export default function Navbar() {
   return (
     <div className="navbar-container">
       <div className="navbar-item">
-        <img src={Logo} alt="Streamflix" />
+        <Link to="/"><img src={Logo} alt="Streamflix" /></Link>
+        <Link to="/">Discover</Link>
+        <Link to="/collection">Collection</Link>
         <MovieContext.Consumer>
-          {(context) => <p>{`Rp ${context.cash.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`}</p>}
+          {(context) => (
+            <div className="coins">
+              <img src={Coins} alt="Coins" />
+              <span>{`Rp ${context.cash.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`}</span>
+            </div>
+          )}
         </MovieContext.Consumer>
         <p>Hello, Malik!</p>
       </div>
