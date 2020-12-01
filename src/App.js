e5dfbef59e1e13c61a463a8e148e6f7c03f83259
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 23:21:05 +0700

    reset route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div>
       <MovieProvider>
         <Switch>
-          <Route path={`${process.env.PUBLIC_URL}/`} component={Main} />
+          <Route exact path="/" component={Main} />
           <Route path="/page/:page" component={Main} />
           <Route path="/collection" component={Collection} />
           <Route path="/:movieID/:slug" component={Details} />
