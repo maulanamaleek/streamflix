b47efe5c6fe3ca984156774d1f18a3651639966f
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-12-01 10:49:42 +0700

    add rating

--- src/components/molecules/display/index.js
+++ src/components/molecules/display/index.js
@@ -2,22 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Poster, Title } from '../../atoms';
+import Star from '../../../assets/star.png';
 import './displayMovies.scss';
 
 export default function Display({
-  src, title, price, click,
+  src, movie, price, click, slug,
 }) {
   return (
     <div className="display-movies">
       <div className="movie-card">
         <>
           <Poster className="poster" src={src} />
-          <Title className="title" title={title} />
+          <Title className="title" title={movie.title} />
+          <div className="rating">
+            <img src={Star} alt="rating" />
+            <span>{movie.vote_average}</span>
+          </div>
           <p>
             Rp
             {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
           </p>
-          <Link to="/details">
+          <Link to={`/${movie.id}/${slug}`}>
             <button type="button" className="button" onClick={click}>Details</button>
           </Link>
         </>
@@ -28,7 +33,8 @@ export default function Display({
 
 Display.propTypes = {
   src: PropTypes.node.isRequired,
-  title: PropTypes.node.isRequired,
+  movie: PropTypes.node.isRequired,
   price: PropTypes.node.isRequired,
   click: PropTypes.node.isRequired,
+  slug: PropTypes.node.isRequired,
 };
